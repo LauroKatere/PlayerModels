78ebf358a4157d5ce04c64c8ff67041cf3bcefff
Lauro <phixho@gmx.de>
2020-03-22 17:55:57 +0100

    PlayerModelType2 code change
    write the first code for the PlayerModel

--- PlayerModel/Player/PlayerModels/PlayerModelType2.java
+++ PlayerModel/Player/PlayerModels/PlayerModelType2.java
@@ -1,9 +1,60 @@
 package PlayerModels;
  public class PlayerModelType2 extends PlayerModelType1{
 	 
-	 int lifepoints, attackpoints, defensivepoint;
+	 int lifepoints,attackpoint,defensivepoint;
 	 
+	 public PlayerModelType2(String Name) {
+		 super(String Name);
+		 lifepoints = 0;
+		 attackpoint = 0;
+		 defensivepoint = 0;
+	 }
 	 
+	 public PlayerModelType2(String Name, int Lifepoints, int Attackpoints, int defensivepoints) {
+		 super(String Name);
+		 lifepoints = Lifepoints;
+		 attackpoint = Attackpoints;
+		 defensivepoint = defensivepoints;
+	 }
+	
+	 
+	 public PlayerModelType2(String Name, Group x) {
+		 super(String Name, Group x);
+		 lifepoints = 0;
+		 attackpoint = 0;
+		 defensivepoint = 0;
+	 }
+	 
+	 public PlayerModelType2(String Name, Group x, int Lifepoints, int Attackpoints, int defensivepoints) {
+		 super(String Name, Group x);
+		 lifepoints = Lifepoints;
+		 attackpoint = Attackpoints;
+		 defensivepoint = defensivepoints;
+	 }
+	
+	 
+		public void setattackpoints(int Attackpoints) {
+			attackpoint= Attackpoints;
+		}
+		public void setlifepoints(int Lifepoints) { 
+			lifepoints= Lifepoints;
+		}
+		
+		public void setdefensivepoints(int Defensivepoints) {
+			defensivepoint = Defensivepoints;
+		}
+		
+		public int getlifepoints() { 
+			return lifepoints;
+		}
+		
+		public int getattackpoints(){ 
+			return attackpoint;
+		}
+		
+		public int getdefensivepoints() {
+			return defensivepoint;
+		}
 	 
 	 
  }
